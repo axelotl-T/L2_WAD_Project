e2c79cbfa82f5051f47268d11e6ba0b2eb9276d8
axelotl-T <axeltan16@gmail.com>
2025-12-02 19:57:02 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const db = require("./services/dbservice.js");
+
+db.connect()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error.message);
+  });
+
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 module.exports = router;
