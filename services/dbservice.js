4bfe3cd5f45cbfe8bdf4f32803610d1cacd3a6c8
axelotl-T <axeltan16@gmail.com>
2025-12-02 19:51:09 +0800

    Update dbservice.js

--- services/dbservice.js
+++ services/dbservice.js
@@ -4,9 +4,13 @@ const user = require("../models/User.js");
 const review = require("../models/Review.js");
 
 let db = {
-    async connect() {
-        try{
-            await mongoose.connect("mongodb://localhost:27017/ShopSwift")
-        }
+  async connect() {
+    try {
+      await mongoose.connect("mongodb://localhost:27017/ShopSwift");
+      return "Connect to Mongo DB";
+    } catch (e) {
+      console.log(e.message);
+      throw new Error("Error connecting to Mongo DB");
     }
-}
+  },
+};
